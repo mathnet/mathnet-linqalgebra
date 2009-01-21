8b36409551920ef2ac416d097ff5716a6fbb74d6
Christoph Ruegg <git@cdrnet.ch>
2009-01-21 23:11:32 +0100

    elementary algebra: extract numerator/denominator.
    Currently only the top expression is considered. Negative powers are ignored. Needs serious extension, but will do for now as naive test

---
+++ src/app/MathNet.Palladium/ExpressionAlgebra/Elementary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace MathNet.Palladium.ExpressionAlgebra
+{
+    public static class Elementary
+    {
+        public static Expression Numerator(Expression quotient)
+        {
+            if(quotient.NodeType != ExpressionType.Divide)
+            {
+                return quotient;
+            }
+
+            BinaryExpression binary = quotient as BinaryExpression;
+            if(binary == null)
+            {
+                ////return quotient;
+                throw new Exception(String.Format("Expected a BinaryExpression, but was {0} instead.", quotient.Type.Name));
+            }
+
+            return binary.Left;
+        }
+
+        public static Expression Denominator(Expression quotient)
+        {
+            if(quotient.NodeType != ExpressionType.Divide)
+            {
+                return Expression.Constant(1);
+            }
+
+            BinaryExpression binary = quotient as BinaryExpression;
+            if(binary == null)
+            {
+                ////return Expression.Constant(1);
+                throw new Exception(String.Format("Expected a BinaryExpression, but was {0} instead.", quotient.Type.Name));
+            }
+
+            return binary.Right;
+        }
+    }
+}
---
+++ src/test/MathNet.Palladium.Test/ExpressionAlgebraElementaryTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using NUnit.Framework;
+
+namespace MathNet.Palladium.Test
+{
+    using MathNet.Numerics;
+    using MathNet.Palladium.ExpressionAlgebra;
+
+    [TestFixture]
+    public class ExpressionAlgebraElementaryTests
+    {
+        [Test]
+        public void TestNumeratorDenominatorOfTernaryProduct()
+        {
+            Expression<Func<double, double>> lambda = x => 2 * x * Trig.Cosecant(x);
+
+            Expression nominator = Elementary.Numerator(lambda.Body);
+            Assert.AreEqual(ExpressionType.Multiply, nominator.NodeType);
+            Assert.IsInstanceOfType(typeof(BinaryExpression), nominator);
+            Assert.AreEqual("((2 * x) * Cosecant(x))", nominator.ToString());
+
+            Expression denominator = Elementary.Denominator(lambda.Body);
+            Assert.AreEqual(ExpressionType.Constant, denominator.NodeType);
+            Assert.IsInstanceOfType(typeof(ConstantExpression), denominator);
+            Assert.AreEqual("1", denominator.ToString());
+        }
+
+        [Test]
+        public void TestNumeratorDenominatorOfBinaryQuotient()
+        {
+            Expression<Func<double, double>> lambda = x => Math.Exp(x) / (2 * x);
+
+            Expression nominator = Elementary.Numerator(lambda.Body);
+            Assert.AreEqual(ExpressionType.Call, nominator.NodeType);
+            Assert.IsInstanceOfType(typeof(MethodCallExpression), nominator);
+            Assert.AreEqual("Exp(x)", nominator.ToString());
+
+            Expression denominator = Elementary.Denominator(lambda.Body);
+            Assert.AreEqual(ExpressionType.Multiply, denominator.NodeType);
+            Assert.IsInstanceOfType(typeof(BinaryExpression), denominator);
+            Assert.AreEqual("(2 * x)", denominator.ToString());
+        }
+
+        [Test]
+        public void TestNumeratorDenominatorOfTernaryQuotient()
+        {
+            Expression<Func<double, double>> lambda = x => x / 2 / Math.PI;
+
+            Expression nominator = Elementary.Numerator(lambda.Body);
+            Assert.AreEqual(ExpressionType.Divide, nominator.NodeType);
+            Assert.IsInstanceOfType(typeof(BinaryExpression), nominator);
+            Assert.AreEqual("(x / 2)", nominator.ToString());
+
+            Expression denominator = Elementary.Denominator(lambda.Body);
+            Assert.AreEqual(ExpressionType.Constant, denominator.NodeType);
+            Assert.IsInstanceOfType(typeof(ConstantExpression), denominator);
+            Assert.AreEqual(Math.PI.ToString(), denominator.ToString());
+        }
+    }
+}
